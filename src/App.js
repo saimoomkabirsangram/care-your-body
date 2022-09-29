d51a4ae74520f1783133478332dd2d722b8e2c9d
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-29 08:31:10 +0600

    some-changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Body from './components/Body/Body.js'
 
 function App() {
   return (
     <div className="App">
-     
+      <Body></Body>
     </div>
   );
 }
+
 
 export default App;
